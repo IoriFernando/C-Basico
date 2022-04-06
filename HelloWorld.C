d9dd55fe4c2b30114437b3da3afe216f678158c6
Iori Fernando <97190926+IoriFernando@users.noreply.github.com>
2022-04-05 22:54:00 -0300

    Create HelloWorld.C

---
+++ HelloWorld.C
@@ -0,0 +1,28 @@
+// Comentário em linhas 
+/* Comentário em paragráfos */
+   
+/* Sempre no começo de arquivos em C é utilizada para delaração
+das bibliotecas que serão utilizadas a padrão é utilizada a <stdio.h> 
+que é usada para cabeçario padrão de entrada e saídas  */
+
+#include <stdio.h>
+
+/* Para qualquer instruções que serão feitas em C são declaradas em funções por
+hora todas as instruções são definida na função chamada main que quando delarada
+é a primeira função a ser chamada em todo arquivo em C.*/
+
+
+int main(){
+		/* Para qualquer instruções que serão feitas em C são declaradas em funções por
+	hora todas as instruções são definida na função chamada main que quando delarada
+	é a primeira função a ser chamada em todo arquivo em C.*/
+
+	printf("Hello Word!");
+	/* printf("  "); é o comando utilizado para exibir mensagens na tela */
+	return 0;
+		/* Nessa função main é declarada como do tipo inteiro "int" por isso ao função retornar 0
+	como saindo o compilardo entende que a função já cumpriu todas as suas intruções por isso 
+	a aplicação e encerrada*/
+}
+
+/* Para executar arquivos em C é criado um arquivo ".exe" que é executado ao ser solicitado */
